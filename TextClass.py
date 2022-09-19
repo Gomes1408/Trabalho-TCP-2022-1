708ff2cd72ac16e040a4ad25411aad514095309d
Gomes1408 <68618279+Gomes1408@users.noreply.github.com>
2022-09-19 14:49:50 -0300

    Criação da classes texto
    Criação de atributos e método construtor. Função de leitura de arquivo texto implementada. Ainda falta implementar a de pegar o texto direto do campo de preenchimento da interface e a função de conversão de texto para MIDI.

---
+++ TextClass.py
@@ -0,0 +1,23 @@
+from mido import Message, MidiFile, MidiTrack
+
+class Text:
+    def __init__(self, text: str, corresponding_MIDI):
+        self.text = text
+        self.corresponding_MIDI = corresponding_MIDI
+
+    def get_text_from_file(self):
+        file = input("\nEscreva o nome de um arquivo existente a ser lido: ")
+        self.text = open(file, 'r', encoding="utf-8")
+    
+    def get_text_from_label(self):
+        print('Hello')
+
+    def text_to_MIDI(self):
+        file = input("\nInsira o nome do novo arquivo MIDI a ser criado: ")
+        midi = MidiFile()
+
+
+        midi.save(file)
+        
+
+
