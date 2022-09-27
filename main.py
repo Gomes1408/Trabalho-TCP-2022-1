4b0b4411c6823c3c3ae5fdbd9967ad071d803dbb
Gdamage <giordano.s.mail@gmail.com>
2022-09-27 02:47:18 -0300

    implementação da janela principal

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from sys import argv
+from MainWindow import MainWindow
 
 def main(argv):
-    print("Ola trabalho!")
-
     if(argv):
         print(f"Recuperou: {argv}")
+    
+    janelaPrincipal = MainWindow(title = "Text2MIDI - Projeto sem nome*")
+    janelaPrincipal.update()
+    janelaPrincipal.mainFrameLoop()
+    janelaPrincipal.mainloop()  # metodo mainloop (tkinter) para atualizar a janela
 
 if(__name__ == "__main__"):
     main(argv)
