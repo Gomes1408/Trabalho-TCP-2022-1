6e8161d8946d1edc4e7be5c7af817dc2c8e7d34a
Gomes1408 <man0g0meta1408@gmail.com>
2022-10-03 13:25:11 -0300

    Update MidiClass.py

--- MidiClass.py
+++ MidiClass.py
@@ -1,6 +1,6 @@
-import pygame
-
-class Midi:
+import tkinter
+import MainWindow as MW
+class Midi(MW):
     def __init__(self, text: str, corresponding_Text):
         self.text = text
         self.corresponding_MIDI = corresponding_Text
@@ -9,6 +9,4 @@ class Midi:
         file = input("\nEscreva o nome de um arquivo existente a ser lido: ")
         self.text = open(file, 'r', encoding="utf-8")
 
-    def MIDI_to_text(self):
-        file = input("\nInsira o nome do novo arquivo MIDI a ser criado: ")
         
